b67e5895974a0052ee5ad3c9970dd87c2f75b75a
PatriktheAstronomer <czech.prime.minister@gmail.com>
2021-09-15 18:55:42 +0200

    ML libs added to the future NN file

---
+++ neural_network.py
@@ -0,0 +1,4 @@
+from sklearn.preprocessing import StandardScale
+from sklearn.neural_network import MLPRegressor # regressor
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
