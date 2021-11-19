82b044776218811882a34c78ddb911fff7c2a7dc
PatriktheAstronomer <czech.prime.minister@gmail.com>
2021-11-19 17:13:02 +0100

    problem w commit

--- neural_network.py
+++
@@ -1,4 +0,0 @@
-from sklearn.preprocessing import StandardScale
-from sklearn.neural_network import MLPRegressor # regressor
-from sklearn.datasets import make_regression
-from sklearn.model_selection import train_test_split
